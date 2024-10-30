d2da857750a2cfadd68485177e07a3e0172c85ce
Devley0 <getkhun@gmail.com>
2024-10-30 11:04:06 +0100

    Updated program.cs

--- Program.cs
+++ Program.cs
@@ -5,12 +5,7 @@ class Program
 {
     public static void Main()
     {
-        int[] windowSize = [800, 800];
-        Raylib.InitWindow(windowSize[0], windowSize[1], "placeholder game");
-        //
-        int[] textBoxSize = [Convert.ToInt32(Raylib.GetScreenWidth() / 1.6f), Convert.ToInt32(Raylib.GetScreenWidth() / 5.3f)];
-        const int textBoxYOffset = 5;
-        int[] defaultTextboxPosition = [Raylib.GetScreenWidth() / 2 - textBoxSize[0] / 2, Raylib.GetScreenHeight() - textBoxSize[1] - textBoxYOffset];
+        Raylib.InitWindow(800, 800, "placeholder game"); //placeholder data
 
         Game game = new();
 
